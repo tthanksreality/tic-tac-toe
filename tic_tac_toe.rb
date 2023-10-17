adc42e3c5f89054b1807b5603b1ec9f9a4c0d734
tthanksreality <gymfreak500@abv.bg>
2023-10-17 11:48:44 +0300

    added row check and column check

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -14,7 +14,7 @@ class Player
   end
 end
 
-# Initializes the board in the terminal, checks for wins, updates the moves
+# Initializes the board in the terminal, checks for wins, updates the moves and checks if a move is valid
 class Board
   def initialize
     @board = Array.new(3) { Array.new(3, '-') }
@@ -25,7 +25,45 @@ class Board
       puts row.join(' | ')
     end
   end
+
+  def valid?(row, col)
+    row.between?(1, 3) && col.between?(1, 3) && @board[row - 1][col - 1] == '-'
+  end
+
+  def update(row, col, symbol)
+    if valid?(row, col)
+      @board[row - 1][col - 1] = symbol
+      true
+    else
+      false
+    end
+  end
+
+  def check_win_row
+    @board.each do |row|
+      row[0] if row.uniq.length == 1 && !row.include?('-')
+    end
+    nil
+  end
+
+  def check_column_win
+    3.times do |col|
+      column = @board.map { |row| row[col] }
+      return column[0] if column.uniq.length == 1 && !column.include?(' ')
+    end
+    nil
+  end
+
 end
 
-milko = Board.new
-milko.display
+murtveca = Board.new
+murtveca.display
+p murtveca.valid?(1, 2)
+murtveca.update(1, 1, 'X')
+murtveca.update(1, 2, 'X')
+murtveca.update(1, 3, 'X')
+murtveca.display
+murtveca.update(2, 1, 'X')
+murtveca.update(3, 1, 'X')
+murtveca.display
+p murtveca.check_column_win
